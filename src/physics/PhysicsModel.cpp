76889152b8d74587dfc4493fc672d07822472693
bartek <bartek.k2906@gmail.com>
2026-04-23 18:32:37 +0200

    feat: mass calculations

--- src/physics/PhysicsModel.cpp
+++ src/physics/PhysicsModel.cpp
@@ -1,6 +1,7 @@
 // PhysicsModel.cpp
 
 #include "physics/PhysicsModel.hpp"
+#include <cmath>
 
 PhysicsModel::PhysicsModel(double gravity, double drag_coefficient)
     : m_gravity{gravity},
@@ -21,7 +22,15 @@ State PhysicsModel::derivative(const State& s, const ThrustCommand& cmd) const {
     deriv.vx = (cmd.fx + drag_x) / s.mass;
     deriv.vy = (cmd.fy + drag_y - m_gravity * s.mass) / s.mass;
 
-    deriv.mass = 0.0;
+    const double fuel_consumption_rate = 0.0005; 
+    double total_thrust = std::sqrt(cmd.fx * cmd.fx + cmd.fy * cmd.fy);
+    
+    // Mass decreases only if there's significant thrust and we have fuel left
+    if (s.mass > 300.0) { // dry mass
+        deriv.mass = -fuel_consumption_rate * total_thrust;
+    } else {
+        deriv.mass = 0.0;
+    }
 
     return deriv;
 }
